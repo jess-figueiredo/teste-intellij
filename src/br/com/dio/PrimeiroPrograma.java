eacc8dffbe7c5466caf4a9ce956c47fe5a3f6534
jess-figueiredo <jhessikafeitosa@hotmail.com.br>
2022-08-03 21:29:53 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,16 +1,45 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import jdk.swing.interop.SwingInterOpUtils;
 
 public class PrimeiroPrograma {
-    public static void main(String[] args) {//crtl+shiftq+f10
-        Gato gato = new Gato();
+        public static void main(String[] args) {//crtl+shiftq+f10
+            Gato gato = new Gato();
 
-        System.out.println(gato);
-        System.out.println(gato);
-// deletar linha crtl+y
-        /*int a = 5;
-        int b = 3;
-        System.out.println("Hello world!"+(a+b));//shift+f10*/
+            System.out.println(gato);
+
+            Livro livro1 = new Livro("O problema das mentes fechadas", 309);
+            System.out.println(livro1);
+        }
+}
+
+class Livro {
+    private String titulo;
+    private Integer numPage;
+
+    //contrutor
+
+    public Livro(String titulo, Integer numPage) {
+        this.titulo = titulo;
+        this.numPage = numPage;
+    }
+
+    //getter nd setter
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(Integer numPage) {
+        this.numPage = numPage;
     }
 }
